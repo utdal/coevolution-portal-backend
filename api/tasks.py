ac0e450ac4561e4fa7061996385f82a0988788ab
David Brock <barefootbrock@gmail.com>
2024-07-22 13:46:54 -0500

    MSA file upload and DI calculation

--- api/tasks.py
+++ api/tasks.py
@@ -1,5 +1,7 @@
 from celery import shared_task
 import time
+from dca.dca_class import dca
+import os
 
 @shared_task
 def get_msa_task(seq):
@@ -7,6 +9,8 @@ def get_msa_task(seq):
     return seq + "\nabc\ndef\nAdditional sequences..."
 
 @shared_task
-def get_DI_pairs_task(seq):
-    time.sleep(15)
-    return [(1, 2), (3, 4), (5, 6)]
+def get_DI_pairs_task(msaPath):
+    protein_family = dca(msaPath)
+    protein_family.mean_field()
+    os.remove(msaPath)
+    return [(int(i), int(j), float(h)) for i, j, h in protein_family.DI]
