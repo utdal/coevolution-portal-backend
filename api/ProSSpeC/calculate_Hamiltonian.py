571c0c37b634b6b6505e1843d9578133a4961472
ceziegler <cheyenneeziegler@gmail.com>
2025-02-22 18:27:41 -0600

    sped up Ham calc

--- api/ProSSpeC/calculate_Hamiltonian.py
+++ api/ProSSpeC/calculate_Hamiltonian.py
@@ -27,17 +27,24 @@ aa2num = {
 
 def calc_Hamiltonian(seq_list, coupling_tbl, lf_tbl):
     H = np.zeros(len(seq_list))
+    
     for seq_idx, seq in enumerate(seq_list):
-        nums = [aa2num[aa] for aa in seq]
-
-        # Add local fields
-        H[seq_idx] += sum([lf_tbl.iloc[num,num_idx] for num_idx, num in enumerate(nums)]) 
-
-        # Add couplings
-        for num_idx in range(0, len(nums)):
-            i = 21*(num_idx)+nums[num_idx]
-            for pair in range(num_idx+1,len(nums)):
-                j = 21*(pair)+nums[pair]
-                H[seq_idx] += coupling_tbl.iloc[i,j]
+        nums = np.array([aa2num[aa] for aa in seq])
+        
+        # Add all local fields at once using numpy operations
+        H[seq_idx] += np.sum([lf_tbl.iloc[num, pos] for pos, num in enumerate(nums)])
+        
+        # Compute all coupling pairs at once
+        positions = np.arange(len(nums))
+        i_indices = [21 * pos + nums[pos] for pos in positions]
+        
+        # Create all pairs of positions efficiently
+        pairs = [(i, j) for i in range(len(nums)) for j in range(i + 1, len(nums))]
+        j_indices = [21 * pair[1] + nums[pair[1]] for pair in pairs]
+        i_indices_pairs = [21 * pair[0] + nums[pair[0]] for pair in pairs]
+        
+        # Sum all coupling terms at once
+        H[seq_idx] += np.sum([coupling_tbl.iloc[i, j] for i, j in zip(i_indices_pairs, j_indices)])
+    
     return -H
 
