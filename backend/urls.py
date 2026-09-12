9cd03929020145366253f9be9e4c81f8205bd18a
RaheelSyedAhmed <raheelsyedahmed@gmail.com>
2026-09-12 01:49:46 -0500

    Added api routing for static files and admin.

--- backend/urls.py
+++ backend/urls.py
@@ -20,7 +20,8 @@ from django.conf import settings
 from django.views.static import serve
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/', include('api.urls')),
     re_path(r'^api/media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^api/static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
